26ad120b28027bdc695f64219f47ca69055d32f2
Gerd Reiss <gerd@reiss.pro>
2020-11-19 18:48:45 +0100

    Udemy - The Python Mega Course: refactor + more read/write files

--- udemy/mega/files/main.py
+++ udemy/mega/files/main.py
@@ -1,19 +1,30 @@
+import os
+import sys
+
+
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # f = open("fruits.txt")
-    # content = f.read()
-    # f.close()
-
+def read_file_if_exists(path):
     print("=" * 50)
+    print("read_file_if_exists:")
+    if os.path.exists(path):
+        with open(path) as f:
+            print(f.read())
+    else:
+        print("File does not exist.")
 
+
+def read_fruits():
+    global f, content
+    print("=" * 50)
     with open("data/fruits.txt") as f:
         content = f.read()
-
     print(content)
     print(len([letter for letter in content if letter == 'a']))
 
-    print("=" * 50)
 
+def write_veggies():
+    global f
+    print("=" * 50)
     with open("data/veggies.txt", "a+") as f:
         f.write("cucumber\n")
         f.write("eggplant\n")
@@ -21,11 +32,25 @@ if __name__ == '__main__':
         f.seek(0)
         print(f.read())
 
-    print("=" * 50)
 
+def duplicate_veggies():
+    global f, content
+    print("=" * 50)
     with open("data/fruits.txt", "a+") as f:
         f.seek(0)
         content = f.read()
         print(content)
         f.write(content)
         f.write(content)
+
+
+if __name__ == '__main__':
+    # f = open("fruits.txt")
+    # content = f.read()
+    # f.close()
+
+    # read_fruits()
+    # write_veggies()
+    # duplicate_veggies()
+
+    read_file_if_exists("data/fruits.txt")
